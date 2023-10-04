87461ac58b3b73530196be633003c2fb4dfe12a9
heitor <heitorleonnyyt1234@gmail.com>
2023-10-03 21:46:45 -0300

    Resolvendo dois exercicios
    Signed-off-by: heitor <heitorleonnyyt1234@gmail.com>

---
+++ src/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,39 @@
+package set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos(){
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade){
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome(){
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutosPorPreco(){
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+        cadastroProdutos.adicionarProduto(9L, "Arroz", 3.5, 1);
+        cadastroProdutos.adicionarProduto(9L, "Feijão", 3.0, 1);
+        cadastroProdutos.adicionarProduto(5L, "Feijão", 3.5, 1);
+        cadastroProdutos.adicionarProduto(3L, "Macarrão", 4.5, 3);
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+
+}
